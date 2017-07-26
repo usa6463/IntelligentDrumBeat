9737c7455ea8698700a817e00a82f7daa1d984ba
Red_Maple <namutip22@gmail.com>
2017-07-26 23:18:24 +0900

    Update test_dw.py

--- data/test_dw.py
+++ data/test_dw.py
@@ -10,7 +10,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 			,'accept-encoding': 'gzip,deflate'};
 http = urllib3.PoolManager();
 
-r = http.request('GET', 'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/download/ HTTP/1.1');
+r = http.request('GET', 'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/download/ HTTP/1.1',headers=headers);
 
 r.data;
 #cookies = {'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'};
