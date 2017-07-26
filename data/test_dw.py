4521a51a6daf6fa24e766be8c008322d3a011f0f
Red_Maple <namutip22@gmail.com>
2017-07-26 23:08:15 +0900

    collect
    help..

---
+++ data/test_dw.py
@@ -0,0 +1,16 @@
+import urllib3
+import os
+import sys
+import json
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+			,'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'
+			,'Referer':'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/'
+			,'connection': 'keep-alive'
+			,'accept-encoding': 'gzip,deflate'};
+http = urllib3.PoolManager();
+
+r = http.request('GET', 'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/download/ HTTP/1.1');
+
+r.data;
+#cookies = {'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'};
