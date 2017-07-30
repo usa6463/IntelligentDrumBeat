a214a11d8e2a324140eacc53ecafe6adfd448d2c
Red_Maple <namutip22@gmail.com>
2017-07-31 01:06:54 +0900

    복원중..
    미안해 내가 지금 가족 휴가 때문에 빨리 끝내려고 했는데 갔다와서 해야할듯.. 2일에오니 다시 완성할게 ㅠ

--- data/test_dw.py
+++ data/test_dw.py
@@ -2,16 +2,28 @@ import urllib3
 import os
 import sys
 import json
+from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+headers = {'accept-encoding': 'gzip,deflate'
+			,'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
 			,'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'
 			,'Referer':'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/'
 			,'connection': 'keep-alive'
-			,'accept-encoding': 'gzip,deflate'
-	  		,'Host':'guitarprotabs.org'};
+			,'Host':'guitarprotabs.org'};
 http = urllib3.PoolManager();
+host = 'http://guitarprotabs.org'
+get_url = '/a/a_dominique/au_22_bar_33/download/ HTTP/1.1'
+root = 'C:/Users/Hyunwoo/Desktop/files/';
 
-r = http.request('GET', 'http://guitarprotabs.org/a/a_dominique/au_22_bar_33/download/ HTTP/1.1',headers=headers);
+print(soup.find_all("li", {}));
+
+r = http.request('GET', host+get_url ,headers=headers);
+file_name = '';
+
+if os.path.exists(root) == False:
+	os.makedirs(root);
+fp = open(root+'test.gp3','wb');
+fp.write(r.data);
 
 r.data;
 #cookies = {'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'};
