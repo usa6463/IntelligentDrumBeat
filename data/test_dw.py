cf7458f82376f0dafd54cb5089f7939213cb9f07
Red_Maple <namutip22@gmail.com>
2017-08-03 20:00:02 +0900

    'A' data 다운로드
    우선 a만 전부 다운이 되는 걸 확인 코드를 가다듬을 필요가 있음.  나머지는 아스키로 알파벳 끝까지.

--- data/test_dw.py
+++ data/test_dw.py
@@ -15,10 +15,32 @@ host = 'http://guitarprotabs.org'
 get_url = '/a/a_dominique/au_22_bar_33/download/ HTTP/1.1'
 root = 'C:/Users/Hyunwoo/Desktop/files/';
 
-print(soup.find_all("li", {}));
-
+alpha = 'a';
+Parser_url = host + '/' + alpha + '/';z
+url_temp = Parser_url + '/1';
+html_page = http.request('GET', url_temp);
+page_txt = html_page.data;
+bs = BeautifulSoup(page_txt,'html.parser');
+for test in bs.tbody.find_all('a'):
+		url_temp2 = test.get('href');
+		html_page_temp = http.request('GET',url_temp2);
+		page_txt2 = html_page_temp.data;
+		bs2 = BeautifulSoup(page_txt2,'html.parser');
+		for test2 in bs2.tbody.find_all('a'):
+			download_url = test2.get('href');
+			file_name = test2.get_text();
+			get_url = download_url+'download/ HTTP/1.1';
+			r = http.request('GET', get_url, headers=headers);
+			for test3 in bs2.tbody.find_all('td')[1]:
+				if os.path.exists(root) == False:
+					os.makedirs(root);
+				fp = open(root+file_name+test3,'wb');
+				fp.write(r.data);
+				fp.close();
+#print(soup.find_all("li", {}));
+'''
 r = http.request('GET', host+get_url ,headers=headers);
-file_name = '';
+
 
 if os.path.exists(root) == False:
 	os.makedirs(root);
@@ -26,4 +48,5 @@ fp = open(root+'test.gp3','wb');
 fp.write(r.data);
 
 r.data;
+'''
 #cookies = {'Cookie':'PHPSESSID=c6ce8b64b6a31ace4645b0225ceb4e7c'};
