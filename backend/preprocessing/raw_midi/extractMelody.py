ec4b0ab2e3622e847e1cc85f4dcf41df586f3adb
usa6463 <usa6463@pukyong.ac.kr>
2017-08-30 22:48:10 +0900

    add sample function

--- backend/preprocessing/raw_midi/extractMelody.py
+++ backend/preprocessing/raw_midi/extractMelody.py
@@ -34,33 +34,17 @@ def sample(file_name, indices):
     extracted_melody = pretty_midi.PrettyMIDI()
     generated_melody = pretty_midi.Instrument(program=1) # Acoustic Grand Piano
     generated_melody.is_drum = False
+    generated_melody.name = 'Melody'
 
     melody_inst = []
     for i in indices:
         melody_inst.append(midi_data.instruments[int(i)])
 
-    beats = midi_data.get_downbeats()
-    for i in range(len(beats)-1):
-        start = beats[i]
-        end = beats[i+1]
-        interval = float((end-start)) / separate_power
+    for inst in melody_inst:
+        notes = inst.notes
+        for note in notes:
+            generated_melody.notes.append(note)
         
-        part = []
-        for j in range(separate_power):
-            part.append(start)
-            start += interval
-        part.append(end)
-        
-        for time_i in range(len(part)-1):
-            for melody in melody_inst:
-                for note in melody.notes:
-                    if (note.start > part[time_i]) and (note.start < part[time_i+1]):
-                        new_note = pretty_midi.Note(velocity=100,
-                            pitch=note.pitch,
-                            start=part[time_i],
-                            end=part[time_i]+.3)
-                        generated_melody.notes.append(new_note)
-
     extracted_melody.instruments.append(generated_melody)
     extracted_melody.write(file_name[:-4] + '_extract' + '.mid')
 
@@ -75,38 +59,33 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print usage
         exit()
-    
-    try:
-        if sys.argv[1] == 'c':
-            if not sys.argv[2]:
-                print usage
-                exit()
-            file_name = sys.argv[2]
-            check(file_name)
-            
-        elif sys.argv[1] == 's':
-            if not sys.argv[2] or not sys.argv[3]:
-                print usage
-                exit()
-            file_name = sys.argv[2]
-            indices = []
-            for i in range(3, len(sys.argv)):
-                indices.append(sys.argv[i])
-            sample(file_name, indices)
-            
-        elif sys.argv[1] == 'e':
-            if not sys.argv[2] or not sys.argv[3]:
-                print usage
-                exit()
-            file_name = sys.argv[2]
-            indices = []
-            for i in range(3, len(sys.argv)):
-                indices.append(sys.argv[i])
-            extract(file_name, indices)
 
-        else:
+    if sys.argv[1] == 'c':
+        if not sys.argv[2]:
             print usage
+            exit()
+        file_name = sys.argv[2]
+        check(file_name)
+        
+    elif sys.argv[1] == 's':
+        if not sys.argv[2] or not sys.argv[3]:
+            print usage
+            exit()
+        file_name = sys.argv[2]
+        indices = []
+        for i in range(3, len(sys.argv)):
+            indices.append(sys.argv[i])
+        sample(file_name, indices)
+        
+    elif sys.argv[1] == 'e':
+        if not sys.argv[2] or not sys.argv[3]:
+            print usage
+            exit()
+        file_name = sys.argv[2]
+        indices = []
+        for i in range(3, len(sys.argv)):
+            indices.append(sys.argv[i])
+        extract(file_name, indices)
 
-    except Exception as ex:
-        print ex
+    else:
         print usage
