af1ce9901a4dc2dc78297c679b519a56f458fe53
usa6463 <usa6463@pukyong.ac.kr>
2017-08-30 23:24:09 +0900

    add move function

--- backend/preprocessing/raw_midi/extractMelody.py
+++ backend/preprocessing/raw_midi/extractMelody.py
@@ -7,9 +7,9 @@ Usage
 ----------------------------------------------------------------------------------
 python extractMelody.py c filename  # for check the program number of instruments
 python extractMelody.py s filename indexnumber(can multi num) # make sample  
-python extractMelody.py e filename indexnumber(can multi num) # remove all mid file in ./raw_midi and make extracted midi in ./midi
+python extractMelody.py m filename # move extracted midi to ../midi
 ----------------------------------------------------------------------------------
-    '''
+'''
 separate_power = 32
 
 def check(file_name):
@@ -32,9 +32,20 @@ def sample(file_name, indices):
 
     midi_data = pretty_midi.PrettyMIDI(file_name)
     extracted_melody = pretty_midi.PrettyMIDI()
+
     generated_melody = pretty_midi.Instrument(program=1) # Acoustic Grand Piano
     generated_melody.is_drum = False
     generated_melody.name = 'Melody'
+
+    generated_durm = pretty_midi.Instrument(program=0) 
+    generated_durm.is_drum = True
+    generated_durm.name = 'Drum'
+
+    for inst in midi_data.instruments:
+        if inst.is_drum:
+            notes = inst.notes
+            for note in notes:
+                generated_durm.notes.append(note)
 
     melody_inst = []
     for i in indices:
@@ -46,11 +57,19 @@ def sample(file_name, indices):
             generated_melody.notes.append(note)
         
     extracted_melody.instruments.append(generated_melody)
+    extracted_melody.instruments.append(generated_durm)
     extracted_melody.write(file_name[:-4] + '_extract' + '.mid')
 
-def extract(file_name, indices):
-    print 'extract : ' + file_name
-    print indices
+def move():
+    filenames = os.listdir('.')
+    sample_filenames = [f for f in filenames if f.endswith('_extract.mid')]
+    original_filenames = [f[:-12] + '.mid' for f in sample_filenames]
+
+    for sample_name in sample_filenames:
+        os.rename(sample_name, '../midi/'+sample_name)    
+    for file_name in original_filenames:
+        if os.path.exists(file_name):
+            os.remove(file_name)
 
 if __name__ == '__main__':
     if os.path.exists('../midi/') == False:
@@ -77,15 +96,8 @@ if __name__ == '__main__':
             indices.append(sys.argv[i])
         sample(file_name, indices)
         
-    elif sys.argv[1] == 'e':
-        if not sys.argv[2] or not sys.argv[3]:
-            print usage
-            exit()
-        file_name = sys.argv[2]
-        indices = []
-        for i in range(3, len(sys.argv)):
-            indices.append(sys.argv[i])
-        extract(file_name, indices)
+    elif sys.argv[1] == 'm':
+        move()
 
     else:
         print usage
