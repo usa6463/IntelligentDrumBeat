20f4a3236e16e1b95ce599022e2afb8d885be0e6
usa6463 <usa6463@pukyong.ac.kr>
2017-08-31 11:00:35 +0900

    add drum_text_to_midi

--- backend/preprocessing/preprocess.py
+++ backend/preprocessing/preprocess.py
@@ -5,6 +5,46 @@ from config import *
 
 separate_power = 32
 
+def drum_text_to_midi(drum_dir, f, midi_dir):
+    global separate_power
+
+    for file_name in f:
+        midi_data = pretty_midi.PrettyMIDI(midi_dir + '/' + file_name[:-4] + '.mid')
+        extracted_drum = pretty_midi.PrettyMIDI()
+        generated_drum = pretty_midi.Instrument(program=0)
+        generated_drum.is_drum = True
+
+        fd = open(drum_dir+file_name, 'r')
+        txt = fd.read()
+        txt_list = txt.split(' ')[1:-2]
+
+        beats = midi_data.get_downbeats()
+        for i in range(len(beats)-1):
+            start = beats[i]
+            end = beats[i+1]
+            interval = float((end-start)) / separate_power
+            
+            part = []
+            for j in range(separate_power):
+                part.append(start)
+                start += interval
+            part.append(end)
+            
+            for time_i in range(len(part)-1):
+                txt_part = txt_list[i*separate_power+time_i]
+                for char_i in range(len(txt_part)):
+                    if txt_part[char_i] == '1':
+                        new_note = pretty_midi.Note(velocity=100,
+                            pitch=allowed_pitch[char_i],
+                            start=part[time_i],
+                            end=part[time_i]+.3)
+                        generated_drum.notes.append(new_note)
+
+        extracted_drum.instruments.append(generated_drum)
+        extracted_drum.write('test/' + file_name[:-4] + '.mid')
+        fd.close()
+        
+
 def extract_drum(midi_dir, f):
     global separate_power
     midi_data = pretty_midi.PrettyMIDI(midi_dir + '/' + f)
@@ -213,8 +253,22 @@ if __name__ == '__main__':
     midi_filenames = [f for f in midi_filenames if f.endswith('.mid')]
     midi_filenames = [f for f in midi_filenames if os.path.getsize(midi_dir + '/' + f) != 0]
 
-    for f in midi_filenames:
-        drum_midi_to_text(midi_dir, f)
-        melody_midi_to_text(midi_dir, f)
+    # for f in midi_filenames:
+    #     drum_midi_to_text(midi_dir, f)
+    #     melody_midi_to_text(midi_dir, f)
+
+    drum_dir = 'drum/'
+    drum_text_filenames = os.listdir(drum_dir)
+    drum_text_filenames = [f for f in drum_text_filenames if f.endswith('.txt')]
+    
+    melody_dir = 'melody/'
+    melody_text_filenames = os.listdir(melody_dir)
+    melody_text_filenames = [f for f in melody_text_filenames if f.endswith('.txt')]
+    
+    for f in drum_text_filenames:
+        drum_text_to_midi(drum_dir, drum_text_filenames, midi_dir)
+
+    # for f in melody_text_filenames:
+    #     melody_text_to_midi(melody_dir, melody_text_filenames)
 
     print 'done! for %d files' % len(midi_filenames)
