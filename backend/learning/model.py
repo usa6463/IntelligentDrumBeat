1b4247e8168933f2b98eb15ad40ae1e62a902c6d
usa6463 <usa6463@pukyong.ac.kr>
2017-09-03 00:22:14 +0900

    divide text to arr

--- backend/learning/model.py
+++ backend/learning/model.py
@@ -16,35 +16,12 @@ nb_epoch = 1
 loss = 'categorical_crossentropy'
 optimizer = 'adam'
 
-def train_text_to_arr(time_num, case_num):
-    melody_fd = open('../preprocessing/melody_train.txt')
-    melody_txt = melody_fd.read()
-    melody = melody_txt.split(' ')[:-1]
-    melody_fd.close()
-
-    drum_fd = open('../preprocessing/drum_train.txt')
-    drum_txt = drum_fd.read()
-    drum = drum_txt.split(' ')[:-1]
-    drum_fd.close()
-
-    if len(drum) != len(melody):
-        print('train file has the problem')
-
-    song_start = []
-    song_end = []
-    print('trying to count song num...')
-    for i in range(len(melody)):
-        if melody[i] == 'start':
-            song_start.append(i)
-        if melody[i] == 'end':
-            song_end.append(i)
-    song_num = len(song_start)
-    print('train song num : {}'.format(song_num))
+def train_text_to_arr(time_num, case_num, song_start, song_end):
     print('process text to ndarray')
 
     x_train_data = None
     y_train_data = None
-    for i in tqdm(range(song_num)):
+    for i in tqdm(range(len(song_start))):
         length = song_end[i] - song_start[i] - 1
         x_train = np.zeros((1, time_num, case_num), dtype=np.bool)    
         y_train = np.zeros((1, time_num, case_num), dtype=np.bool)
@@ -74,7 +51,7 @@ def train_text_to_arr(time_num, case_num):
             x_train_data = np.append(x_train_data, x_train, axis=0)
             y_train_data = np.append(y_train_data, y_train, axis=0)
 
-    return x_train_data, y_train_data, song_num
+    return x_train_data, y_train_data
 
 def pred_to_drum_track():
     pass
@@ -152,9 +129,33 @@ def predict(arr):
     return preds
 
 if __name__=='__main__':
-    x_train, y_train, song_num = train_text_to_arr(time_num, case_num)
+    melody_fd = open('../preprocessing/melody_train.txt')
+    melody_txt = melody_fd.read()
+    melody = melody_txt.split(' ')[:-1]
+    melody_fd.close()
 
-    if not os.path.exists('./model.json') or not os.path.exists('./model.h5'):
-        get_model(song_num, time_num, case_num, x_train, y_train)
-    else:
-        update_model(x_train, y_train)
+    drum_fd = open('../preprocessing/drum_train.txt')
+    drum_txt = drum_fd.read()
+    drum = drum_txt.split(' ')[:-1]
+    drum_fd.close()
+
+    if len(drum) != len(melody):
+        print('train file has the problem')
+
+    song_start = []
+    song_end = []
+    print('trying to count song num...')
+    for i in range(len(melody)):
+        if melody[i] == 'start':
+            song_start.append(i)
+        if melody[i] == 'end':
+            song_end.append(i)
+    song_num = len(song_start)
+    print('train song num : {}'.format(song_num))
+
+    for i in range(0, song_num, 100):
+        x_train, y_train = train_text_to_arr(time_num, case_num, song_start[i:i+100], song_end[i:i+100])
+        if not os.path.exists('./model.json') or not os.path.exists('./model.h5'):
+            get_model(song_num, time_num, case_num, x_train, y_train)
+        else:
+            update_model(x_train, y_train)
